b4569ec4e3bd21977590371b9833c4256e02612f
rniochy <lniochy@gmail.com>
2023-10-12 12:13:31 +0100

    Working on Login's Form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,30 +1,49 @@
 import React from "react";
 import styles from "./login.module.css"
 import Link from "next/link";
+import {RiErrorWarningLine} from "react-icons/Ri"
 
 const Login = () => {
     return (
-        <div  className={styles.main_}>
-        <div className={styles.first_} ></div>
-        <form  method="post" className={styles.containerFormSignup}>
-                <div className={styles.containerFormSignup_header}>
-                        {/*  <IMAGE LOGO/> */}
+        <div  className={styles.main_Content}>
+            <div className={styles.firstContent} ></div>
+            <form  method="post" className={styles.containerFormLogin}>
+                <div className={styles.containerFormLoginHeader}>
                         <legend> <h2>Welcome, please sign in</h2> </legend>
-                        {/* <p>Already Have An Acount? <strong> <Linkk href="/login"> Log In </Link></strong> </p> */}
                 </div>
-                     <div className={styles.containerFormSignup_body}>
-                            <div className={styles.container_email} >
-                                <label htmlFor="email">E-mail</label>
-                                <input type="email" maxLength={20} name="email" id="email" required/>
-                            </div>
-                            <div className={styles.container_password}>
-                                <label htmlFor="password">Password</label>
-                                <input type="text" maxLength={20} name="password" id="password" required/>
-                            </div>
-                            </div>
-                            <div className={styles.container_button}>
-                                <button  type="submit">Sign in</button>
-                            </div>
+                <div className={styles.containerFormLoginpBody}>
+                    <div className={styles.notificationLoginFormFieldTop}>
+                        <span> <RiErrorWarningLine /> </span>
+                        <span> Authentication failed check your credencials</span> 
+                    </div>
+                    <div className={styles.containerEmail} >
+                        <label htmlFor="email_username">Username or E-mail</label>
+                        <input type="email_username" maxLength={20} name="email_username" id="email_username" required/>
+                        <span className={styles.notificationLoginFormField}>
+                            <span> <RiErrorWarningLine /> </span> 
+                            <span>Please enter a username or e-mail </span> 
+                        </span>
+                    </div>
+                    <div className={styles.containerPassword}>
+                        <label htmlFor="password">Password</label>
+                        <input  type="text" maxLength={20} name="password" id="password" required/>
+                        <span className={styles.notificationLoginFormField}> 
+                            <span> <RiErrorWarningLine /> </span>
+                            <span>Please enter a password </span> 
+                        </span>
+                    </div>
+                    <div className={styles.containerButton}>
+                        <button  type="submit">Sign in</button>
+                    </div>
+                </div>
+            <div className={styles.opctionsBottomOfForm}> 
+                <p>Password Manager and Addicional Options</p>
+                <ul>
+                    <li>Password Manager</li>
+                    <li>Signin with Jasen Identity</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>              
         </form>
     </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-// import Image from 'next/image'
-import LeadingPage from './leadingPage/page'
 import styles from './page.module.css'
-
-
+import Login from './login/page'
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
-    <LeadingPage />
+      <Login />
     </main>
   )
 } 
