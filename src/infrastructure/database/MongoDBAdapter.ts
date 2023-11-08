b234757068bd932a27a11933d434b24408044fc8
rniochy <lniochy@gmail.com>
2023-11-08 16:49:57 +0100

    creating: Clean architecture features to system

---
+++ src/infrastructure/database/MongoDBAdapter.ts
@@ -0,0 +1,22 @@
+import { MongoClient } from 'mongodb';
+import Connection from './connection';
+
+export default class MongoDBConnection implements Connection {
+ private client: MongoClient;
+
+ constructor(private uri: string) {
+   this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+ }
+
+ async query(statement: string, params: any): Promise<any> {
+   await this.client.connect();
+   const db = this.client.db();
+   const result = await db.collection(statement).find(params).toArray();
+   await this.client.close();
+   return result;
+ }
+
+ async close(): Promise<void> {
+   await this.client.close();
+ }
+}
---
+++ src/infrastructure/repository/database/userRepositoryDatabase.ts
@@ -0,0 +1,10 @@
+import Connection from "../../database/connection";
+
+class userRepositoryDataBase {
+
+    constructor(readonly connection: Connection){
+    }
+    
+}
+
+export default userRepositoryDataBase;
